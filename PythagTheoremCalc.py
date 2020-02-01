65835d5855c01c37f0baf5ce4e390dabd3013bb7
Nigleto <58761175+Nigleto@users.noreply.github.com>
2020-01-31 20:16:43 -0800

    Create PythagTheoremCalc.py

---
+++ PythagTheoremCalc.py
@@ -0,0 +1,8 @@
+question = 'yes'
+while (question == 'yes'):
+  epic = input("Enter an a.")
+  epicc = input("Enter a B.")
+  answer = float(epic)*float(epic) + float(epicc)*float(epicc)
+  print(answer)
+  question = input('Would you like to do this again?')
+  
